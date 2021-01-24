ca9365beb518c225fc89ab642729b0f7936ea3f5
luisricardo494 <luisricardo494@gmail.com>
2021-01-24 18:16:16 -0500

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -45,10 +45,10 @@ app.use('/uploads', uploadRoute);
 // app.use('/users', userRoute);
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-// app.use(express.static(path.join(__dirname, '/../frontend/build')));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/../frontend/build/index.html'));
-// });
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+});
 
 
 app.listen(port, () => {console.log(`Server started at ${port}`)});
